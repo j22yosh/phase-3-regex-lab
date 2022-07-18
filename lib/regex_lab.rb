7a51257c676cfc4af2d70496e1b080895abac639
Joshua Welsh <j22yosh@gmail.com>
2022-07-17 23:57:25 -0400

    turning in what i have

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,24 @@
 def starts_with_a_vowel?(word)
+    case(word)
+    when word.scan(/\b[aeiou][a-z]*\b/ )
+    true
+    else word.scan(/\b^[aeiou][a-z]*\b/ )
+    false
 
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
+    text.scan(/\b[a-zA-Z]{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.scan(/\b^[A-Z]+$*\b/ )
 end
 
 def valid_phone_number?(phone)
